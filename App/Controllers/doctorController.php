52c14c113a4f18bc4e833ad0aefd670bc57c9f55
toto rubianto <toto.rubianto.17@gmail.com>
2019-03-17 14:50:20 +0700

    poliklinik

--- App/Controllers/doctorController.php
+++ App/Controllers/doctorController.php
@@ -5,18 +5,18 @@ namespace App\Controllers;
 use App\Core\Controller,
 App\Models\Hospital;
 
-class hospitalController extends Controller {
+class doctorController extends Controller {
 
     public function __construct() {
 		$this->Hospital = new Hospital();
 	}
 
-	public function index(){
-        $result=$this->Hospital->hospital();
+    public function index(){
+        $result=$this->Hospital->poliklinik();
         $fetch=mysqli_fetch_all($result,MYSQLI_ASSOC);
-        $data=array('hospital' => $fetch);
+        $data=array('poliklinik' => $fetch);
         echo json_encode($data);
     }
- 
+  
 
 }
